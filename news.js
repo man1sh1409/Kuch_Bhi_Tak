8cc2dab0a8edf33449703a08b5093ceda061357a
man1sh1409 <55057062+man1sh1409@users.noreply.github.com>
2022-07-02 11:17:58 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,10 +1,10 @@
 const apikey = "716f0370335b45028b4ae489f1549fc8";
-const source ='the-hindu'
-const url = `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=716f0370335b45028b4ae489f1549fc8`;
+const source ='bbc-news'
+const Url = `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=716f0370335b45028b4ae489f1549fc8`;
 const container=document.getElementsByClassName('container1');
 const breakingNewsUrl='https://newsapi.org/v2/top-headlines?country=in&apiKey=716f0370335b45028b4ae489f1549fc8'
 
-fetch(breakingNewsUrl)
+fetch(Url)
   .then(function (response) {
     const data = response.json();
     return data;
